80da09ff0c19c42730bba46db5de9128cf49f22e
Joffily Ferreira <joffily@outlook.com>
2016-03-09 23:15:40 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# IFPB news Crawler
+
+A simple web crawler for fetch IFPB's news and store it with Elastic Search indexer.
+This example was made for discipline of Relational Databases II with a teacher Damires Yluska Souza Fernandes.
+
+## Demo
+
+[![asciicast](https://asciinema.org/a/6vk3453lzuuggfgjec7wghdst.png)](https://asciinema.org/a/6vk3453lzuuggfgjec7wghdst)
+
+Credits: [Marianna Soares Veríssimo](http://github.com/mariannave)
---
+++ scrape.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"github.com/PuerkitoBio/goquery"
+    "gopkg.in/olivere/elastic.v3"
+)
+
+type News struct  {
+    Title string `json:"title"`
+    Text string `json:"text"`
+    Url string `json:url`    
+}
+
+var count = 0
+
+func Indexer(new *News) {
+    client, err := elastic.NewClient()
+    if err != nil {
+        panic(err)
+    }
+
+    _, err = client.Index().
+        Index("scrape").
+        Type("news").
+        BodyJson(new).
+        Do()
+}
+    
+
+func ParseMain(mainUrl string) {
+	doc, err := goquery.NewDocument(mainUrl) 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find(".vevent.contenttype-news-item").Each(func(i int, s *goquery.Selection) {
+		url, _ := s.Find("a").Attr("href")
+		defer ParseNews(url)
+	})
+}
+
+func ParseNews(url string) {
+	document, err := goquery.NewDocument(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	title := document.Find("h1 span").Text()
+    text := ""
+    
+    document.Find("#parent-fieldname-text p").Each(func(i int, s *goquery.Selection) {   
+        text = text + s.Text()  
+    })
+    
+    news := News{
+        Title: title,
+        Text: text,
+        Url: url,
+    }
+    Indexer(&news)
+    
+    count += 1
+    fmt.Printf("> %d) %s.\n> URL: %s\n", count, strings.Trim(title, " \r \n"), url)
+}
+
+func main() {
+
+	for i := 0; i < 25; i += 5 {
+		root := "http://www.ifpb.edu.br/"
+		path := "campi/noticias-campi/folder_summary_view?b_start:int="
+		fullUrl := root + path + fmt.Sprintf("%d", i)
+
+    ParseMain(fullUrl)
+	}
+}
